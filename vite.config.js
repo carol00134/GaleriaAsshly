9cb6e784f61d808e6f8e4392748d2da0395f0094
Genesis <carolmontoya0314@gmail.com>
2024-03-19 23:40:21 -0600

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import hbsContext  from './context/indexPage';
 export default defineConfig(
     {
-        base: '/GaleriaAsshly/',
+        base: '/Galeria/',
         appType: 'mpa',
         build: {
             rollupOptions: {
